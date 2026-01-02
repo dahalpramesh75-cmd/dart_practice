e9e2676916e8e746c6fe16a881d01b33bc7ca648
Pramesh <dahalpramesh75@gmail.com>
2026-01-02 17:33:08 +0545

    1st commit

---
+++ bin/second.dart
@@ -0,0 +1,14 @@
+void main(){
+  print("Hello world from second");
+  int a = 1;
+  String b = "Pramesh";
+  double c = 1.1;
+  bool d = True;
+  final String e = "HSA";
+  final int f = 1;
+  const pie = 3.14;
+  var g = 1;
+  var h = "abcdef"; 
+   
+}
+
